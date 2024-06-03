8851175a4843d63fe3e29e30d7b48b276eef0a51
reelkzap <ricardozapanta.rz@gmail.com>
2024-06-03 18:05:34 +0800

    Update car_rental.php

--- car_rental.php
+++ car_rental.php
@@ -1,55 +1,72 @@
 <?php
 
-//My car rental TABLE
-$car_rental_json=array(
-    [
-        "size" => "Small",
-        "symbol" => "S",
-        "seat_capacity" => 5,
-        "cost" => 5000
-    ],
-    [
-        "size" => "Medium",
-        "symbol" => "M",
-        "seat_capacity" => 10,
-        "cost" => 8000
-    ],
-    [
-        "size" => "Large",
-        "symbol" => "L",
-        "seat_capacity" => 15,
-        "cost" => 12000
-    ],
-);
 
-// Input Field
-$no_of_seats = readline("Please input number (seat): "); 
+class carRental {
 
-// Init global variables
-$cheapest_cost = null;
-$my_stored_values = [];
+    public function computeCheapestRent(int $no_of_seats): array {
 
-// loop car rental json
-foreach($car_rental_json as $car) {
-    $cost = $car["cost"]; //cost per car
+        //My car rental TABLE
+        $car_rental_json=array(
+            [
+                "size" => "Small",
+                "symbol" => "S",
+                "seat_capacity" => 5,
+                "cost" => 5000
+            ],
+            [
+                "size" => "Medium",
+                "symbol" => "M",
+                "seat_capacity" => 10,
+                "cost" => 8000
+            ],
+            [
+                "size" => "Large",
+                "symbol" => "L",
+                "seat_capacity" => 15,
+                "cost" => 12000
+            ],
+        );
 
-    //get multiplier
-    $multiplier = ceil($no_of_seats / $car["seat_capacity"]);
-    //get total cost
-    $total_cost = $multiplier * $cost;
+        // Init variables
+        $cheapest_cost = null;
+        $my_stored_values = [];
 
-    //check if current total cost is the cheapest
-    if($cheapest_cost == null || $total_cost < $cheapest_cost){
-        //update previous value and stored value
-        $cheapest_cost = $total_cost;
-        $my_stored_values = array($car["symbol"], $multiplier, $cheapest_cost);
-    }else{
-        continue;
+        // loop car rental json
+        foreach($car_rental_json as $car) {
+            $cost = $car["cost"]; //cost per car
+
+            //get multiplier
+            $multiplier = ceil($no_of_seats / $car["seat_capacity"]);
+            //get total cost
+            $total_cost = $multiplier * $cost;
+
+            //check if current total cost is the cheapest
+            if($cheapest_cost == null || $total_cost < $cheapest_cost){
+                //update previous value and stored value
+                $cheapest_cost = $total_cost;
+                $my_stored_values = array($car["symbol"], $multiplier, $cheapest_cost);
+            }else{
+                continue;
+            }
+        }
+        return $my_stored_values;
     }
 }
 
-//Display Output
-list($symbol, $count, $cost) = $my_stored_values;
-echo "$symbol x $count \n";
-echo "Total = PHP $cost \n";
+
+//instance
+$car = new carRental();
+// Input Field
+$no_of_seats = readline("Please input number (seat): "); 
+$response = $car->computeCheapestRent($no_of_seats);
+if(!$response) {
+    echo "Something went wrong, please try again.";
+}else{
+    //Display Output
+    list($symbol, $count, $cost) = $response;
+    echo "$symbol x $count \n";
+    echo "Total = PHP $cost \n";
+}
+
+
 ?>
